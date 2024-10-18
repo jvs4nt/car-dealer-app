087fd1d97506a059beed9ed91a2613ec74119533
jvs4nt <joaovit342@gmail.com>
2024-10-18 19:20:43 -0300

    next button + handlemakechange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,9 +26,22 @@ function FilterPage() {
         setYears(yearsArray);
     }, []);
 
+    const handleMakeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedMake(event.target.value);
+    };    
+
     const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedYear(event.target.value);
-      };
+    };
+
+    const handleNextClick = () => {
+        if (selectedMake && selectedYear) {
+            const makeId = makes.find((make) => make.MakeName === selectedMake)?.MakeId;
+            if (makeId) {
+            window.location.href = `/result/${makeId}/${selectedYear}`;
+            }
+        }
+    };
   
   return (
     <div className="container mx-auto p-4">
@@ -68,6 +81,13 @@ function FilterPage() {
             </select>
         </div>
       </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        onClick={handleNextClick}
+        disabled={!selectedMake || !selectedYear}
+      >
+        Next
+      </button>
       
     </div>
   );
