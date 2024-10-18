92581a68207c73be3959f36d29fde6e07856c2dd
jvs4nt <joaovit342@gmail.com>
2024-10-18 18:52:35 -0300

    brand select and fetch on api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,42 @@
+"use client";
+
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
+interface VehicleMake {
+    Make_ID: number;
+    Make_Name: string;
+  }
+
 function FilterPage() {
+    const [makes, setMakes] = useState<VehicleMake[]>([]);
+    const [selectedMake, setSelectedMake] = useState<string | null>(null);
+
+    useEffect(() => {
+        fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
+          .then(response => response.json())
+          .then(data => setMakes(data.Results));
+      }, []);
   
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Vehicle filter</h1>
+      <div>
+        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="make">Brand</label>
+        <select
+            id="make"
+            className="block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            value={selectedMake || ''}
+            
+          >
+            <option value="">Selecione uma marca</option>
+            {makes.map((make) => (
+              <option key={make.Make_ID} value={make.Make_Name}>
+                {make.Make_Name}
+              </option>
+            ))}
+          </select>
+      </div>
     </div>
   );
 }
