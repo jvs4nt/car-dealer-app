282c71ef9d36f2311d9d8a9d1f5491f0912e2e7b
jvs4nt <joaovit342@gmail.com>
2024-10-18 19:42:48 -0300

    migrating router to next/navigation

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
+interface Vehicle {
+    MakeId: number;
+    MakeName: string;
+    Year: number;
+}
 
 function ResultPage() {
-    
+    const router = useRouter();
+    const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+
     return (
         <div className="container mx-auto p-4 mt-10">
             <h1 className="text-3xl font-bold mb-4">Vehicle result</h1>
         </div>
     );
-    }
+}
+
+export default ResultPage;
