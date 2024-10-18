943ad63e49252238bf2348124d6ff856acfedc99
jvs4nt <joaovit342@gmail.com>
2024-10-18 19:34:53 -0300

    result page init

---
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
+function ResultPage() {
+    
+    return (
+        <div className="container mx-auto p-4 mt-10">
+            <h1 className="text-3xl font-bold mb-4">Vehicle result</h1>
+        </div>
+    );
+    }
