426e70192b859ad1063f9a282f4db27cab81ac01
jvs4nt <joaovit342@gmail.com>
2024-10-18 20:58:25 -0300

    removing client side from result screen + suspense component

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -1,8 +1,5 @@
-"use client";
-
-import { useState, useEffect } from 'react';
-import { useParams } from 'next/navigation';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
 interface Vehicle {
     Make_ID: number;
@@ -11,43 +8,39 @@ interface Vehicle {
     Model_Name: string;
 }
 
-function ResultPage() {
-    const { makeId, year } = useParams();
-    const [vehicles, setVehicles] = useState<Vehicle[]>([]);
+async function getVehicles(makeId: string, year: string): Promise<Vehicle[]> {
+    const res = await fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`);
+    const data = await res.json();
 
-    useEffect(() => {
-        if (makeId && year) {
-            fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log("Dados da API:", data);
-                    if (data.Results) {
-                        const vehiclesData = data.Results.map((item: any) => ({
-                            Make_ID: item.Make_ID,
-                            Make_Name: item.Make_Name,
-                            Model_ID: item.Model_ID,
-                            Model_Name: item.Model_Name
-                        }));
+    if (data.Results) {
+        const vehiclesData = data.Results.map((item: any) => ({
+            Make_ID: item.Make_ID,
+            Make_Name: item.Make_Name,
+            Model_ID: item.Model_ID,
+            Model_Name: item.Model_Name
+        }));
 
-                        const uniqueVehiclesData = vehiclesData.filter((vehicle: Vehicle, index: number, self: Vehicle[]) =>
-                            index === self.findIndex((v: Vehicle) => (
-                                v.Model_ID === vehicle.Model_ID
-                            ))
-                        );
+        const uniqueVehiclesData = vehiclesData.filter((vehicle: Vehicle, index: number, self: Vehicle[]) =>
+            index === self.findIndex((v: Vehicle) => (
+                v.Model_ID === vehicle.Model_ID
+            ))
+        );
 
-                        setVehicles(uniqueVehiclesData);
-                    }
-                })
-                .catch(error => {
-                    console.error('Erro ao buscar os veículos:', error);
-                });
-        } else {
-            console.log("makeId and year are not set");
-        }
-    }, [makeId, year]);
+        return uniqueVehiclesData;
+    }
+
+    return [];
+}
+
+interface ResultPageProps {
+    params: { makeId: string; year: string };
+}
+
+const VehiclesList = async ({ makeId, year }: { makeId: string; year: string }) => {
+    const vehicles = await getVehicles(makeId, year);
 
     return (
-        <div className="container mx-auto p-4 mt-10">
+        <div>
             <h1 className="text-3xl font-bold mb-4">Vehicle result for {makeId} - {year}</h1>
             {vehicles.length > 0 ? (
                 <ul>
@@ -58,9 +51,20 @@ function ResultPage() {
                     ))}
                 </ul>
             ) : (
-                <p>Nenhum veículo encontrado para esta marca e ano.</p>
+                <p>No vehicles found for this brand and year.</p>
             )}
+        </div>
+    );
+};
 
+export default function ResultPage({ params }: ResultPageProps) {
+    const { makeId, year } = params;
+
+    return (
+        <div className="container mx-auto p-4 mt-10">
+            <Suspense fallback={<p>Loading vehicles...</p>}>
+                <VehiclesList makeId={makeId} year={year} />
+            </Suspense>
             <Link href={`/`}>
                 <button
                     className={'font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-6 bg-blue-500 hover:bg-blue-700 text-white'}
@@ -71,5 +75,3 @@ function ResultPage() {
         </div>
     );
 }
-
-export default ResultPage;
