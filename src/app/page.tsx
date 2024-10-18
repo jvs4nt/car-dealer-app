a3cf7b3cfb68a60bca7d7c261ec233f6318a53c7
jvs4nt <joaovit342@gmail.com>
2024-10-18 19:04:28 -0300

    years select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,24 @@ interface VehicleMake {
 function FilterPage() {
     const [makes, setMakes] = useState<VehicleMake[]>([]);
     const [selectedMake, setSelectedMake] = useState<string | null>(null);
+    const [selectedYear, setSelectedYear] = useState<string | null>(null);
+    const [years, setYears] = useState<number[]>([]);
 
     useEffect(() => {
         fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json')
           .then(response => response.json())
           .then(data => setMakes(data.Results));
       }, []);
+
+      useEffect(() => {
+        const currentYear = new Date().getFullYear();
+        const yearsArray = Array.from({ length: currentYear - 2015 + 1 }, (_, i) => 2015 + i);
+        setYears(yearsArray);
+    }, []);
+
+    const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedYear(event.target.value);
+      };
   
   return (
     <div className="container mx-auto p-4">
@@ -37,6 +49,24 @@ function FilterPage() {
             ))}
           </select>
       </div>
+      <div className="w-full md:w-1/2 xl:w-1/3 p-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="year">
+            Ano
+          </label>
+          <select
+            id="year"
+            className="block w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            value={selectedYear || ''}
+            onChange={handleYearChange}
+          >
+            <option value="">Selecione um ano</option>
+            {years.map((year) => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+        </div>
     </div>
   );
 }
